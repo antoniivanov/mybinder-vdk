f57db5f0f4bcde5c2726f80ef796ff03685ef643
Antoni Ivanov <aivanov@vmware.com>
2022-02-13 19:06:56 +0200

    add example chart

--- job/10_hello.py
+++ job/10_hello.py
@@ -5,4 +5,6 @@ def run(job_input: IJobInput):
     res = job_input.execute_query("select 1")
     print(res)
 
-    job_input.send_object_for_ingestion(payload={"id": "hi"}, destination_table="hello_table")
+    job_input.send_object_for_ingestion(payload={"id": "Hi"}, destination_table="hello_table")
+    job_input.send_object_for_ingestion(payload={"id": "Hello"}, destination_table="hello_table")
+    job_input.send_object_for_ingestion(payload={"id": "Hola"}, destination_table="hello_table")
