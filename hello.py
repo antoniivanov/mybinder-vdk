372f46daf475ec51d723d723af4d19c7109914c8
Antoni Ivanov <aivanov@vmware.com>
2022-02-24 18:58:40 +0200

    fix binder

--- hello.py
+++ hello.py
@@ -2,20 +2,47 @@ import sqlite3
 import altair as alt
 import pandas as pd
 import streamlit as st
+import os
 
 
-sqliteConnection = sqlite3.connect('/tmp/my-db.db')
+#sqliteConnection = sqlite3.connect('/tmp/my-db.db')
+
+def _connect():
+    from trino import dbapi
+    from trino import constants
+
+
+    from trino.auth import BasicAuthentication
+
+    # auth = (
+    #     BasicAuthentication("user", "pass")
+    # )
+    auth = None
+    conn = dbapi.connect(
+        host=os.environ.get("VDK_TRINO_HOST"),
+        port=int(os.environ.get("VDK_TRINO_PORT")),
+        user="user",
+        auth=auth,
+        catalog=os.environ.get("VDK_TRINO_CATALOG", 'mysql'),
+        schema=os.environ.get("VDK_TRINO_SCHEMA", "default"),
+        http_scheme=constants.HTTP,
+        verify=False,
+        request_timeout=600,
+    )
+    return conn
+
+conn = _connect()
 
 data = pd.read_sql_query(
-    f"select id, count(1) as cnt from hello_table group by id", sqliteConnection
+    f"select id, count(1) as cnt from hello_table group by id", conn
 )
 
 chart = (
     alt.Chart(data)
         .mark_bar()
         .encode(
-            x="id",
-            y=alt.Y("cnt", stack=None),
+        x="id",
+        y=alt.Y("cnt", stack=None),
     )
 )
 st.altair_chart(chart, use_container_width=True)
